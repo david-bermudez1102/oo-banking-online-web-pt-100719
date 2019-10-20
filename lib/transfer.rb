a20e28a6bd40502525e174af047e88f52ce11512
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 00:44:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,6 +28,9 @@ class Transfer
 
   def reverse_transfer
     if @status=="complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
     end
   end
 end
