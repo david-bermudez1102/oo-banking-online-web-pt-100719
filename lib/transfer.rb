078fc8a6e89e39c001bb8eedcc01ef67a90015b5
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 00:38:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status=="pending" && @sender.balance > amount &&valid?
+    if @status=="pending" && @sender.balance > amount && valid?
       @sender.balance -=
     end
   end
